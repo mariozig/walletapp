9405888da3c8f6e7b8dc76ff2a439e152486b839
Mario Zigliotto <mariozig@gmail.com>
2018-08-11 18:09:17 -0700

    Add driver’s license attachment

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -69,6 +69,6 @@ class WalletsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def wallet_params
-      params.require(:wallet).permit(:brand)
+      params.require(:wallet).permit(:brand, :driver_license)
     end
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,2 +1,3 @@
 class Wallet < ApplicationRecord
+  has_one_attached :driver_license
 end
